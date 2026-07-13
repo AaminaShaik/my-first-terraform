6024ec868c03ab64a13c9067e2ae5b00e6793578
Shaik <aamina2599@gmail.com>
2026-07-13 17:58:55 +0530

    day-3-terraform.tfvars,input-variables

---
+++ Day-3-terraform.tfvars/terraform.tfvars
@@ -0,0 +1,3 @@
+ami = "ami-002192a70217ac181"
+instance_type = "t2.medium"
+tags = "my-ec2-instance"
---
+++ Day-3-terraform.tfvars/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+    description = "ami"
+    type = string
+    default = ""
+}
+variable "instance_type" {
+    description = "instance type"
+    type = string
+    default = ""
+}
+variable "tags" {
+    description = "tag"
+    type = string
+    default = ""
+}
