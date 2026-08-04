4ab83b1ccad5e0b859d23f6b595d4f13876ea31a
AaminaShaik <152490512+AaminaShaik@users.noreply.github.com>
2026-08-04 23:22:00 +0530

    Change VPC reference in subnet resource

--- Day-2-terraform-configuration/main.tf
+++ Day-2-terraform-configuration/main.tf
@@ -1,11 +1,11 @@
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "name" {
     cidr_block =var.cidr_block_vpc
     tags = {
       Name=var.tags_vpc
     }
 }
 resource "aws_subnet" "name" {
-    vpc_id = aws_vpc.vpc.id
+    vpc_id = aws_vpc.name.id
     cidr_block =var.cidr_block_subnet
     tags = {
       Name=var.tags_subnet
